56d35f662f83a3f05acc7fbcfe36d7aae6973907
Ryan <hmhuynh0001@zonemail.clpccd.edu>
2025-11-06 17:16:05 -0800

    Stop tracking build/cache folders

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'CARMEN_web_app_v2',
-  webDir: 'www'
-};
-
-export default config;
